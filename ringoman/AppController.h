4f136db0154ea350019ab505c397148941cf9d98
Sota Yokoe <sota@pankaku.com>
2011-07-12 00:21:38 +0900

    Added AppController class.

---
+++ ringoman/AppController.h
@@ -0,0 +1,17 @@
+//
+//  AppController.h
+//  ringoman
+//
+//  Created by Sota Yokoe on 11/07/12.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface AppController : NSObject<NSWindowDelegate> {
+@private
+    
+}
+
+@end
---
+++ ringoman/AppController.m
@@ -0,0 +1,37 @@
+//
+//  AppController.m
+//  ringoman
+//
+//  Created by Sota Yokoe on 11/07/12.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppController.h"
+
+
+@implementation AppController
+
+#pragma mark Window delegate
+
+- (void)windowDidBecomeMain:(NSNotification *)notification {
+    // Check if setting is okay.
+}
+
+#pragma mark - 
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
